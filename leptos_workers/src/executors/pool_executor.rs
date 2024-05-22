d22180759d4f0aa892e3ab4f7a6259bddad71357
Lucas Åström <lucas.astrom00@gmail.com>
2024-05-22 12:48:39 +0200

    Send/Sync adaptations for leptos 0.7

--- leptos_workers/src/executors/pool_executor.rs
+++ leptos_workers/src/executors/pool_executor.rs
@@ -4,10 +4,9 @@ use crate::workers::ChannelWorker;
 use crate::workers::FutureWorker;
 use crate::workers::StreamWorker;
 use crate::workers::WebWorker;
-use alloc::rc::{Rc, Weak};
 use futures::{Stream, StreamExt};
-use std::cell::RefCell;
 use std::future::Future;
+use std::sync::{Arc, Mutex, Weak};
 use wasm_bindgen_futures::spawn_local;
 
 /// This executor will run requests on the first available worker in a pool.
@@ -15,7 +14,7 @@ use wasm_bindgen_futures::spawn_local;
 /// The pool is created with an initial size, but is allowed to expand infinitely if necessary in order to not block.
 #[derive(Debug, Clone)]
 pub struct PoolExecutor<W: WebWorker> {
-    workers: RefCell<Vec<Rc<RefCell<WorkerPoolState<W>>>>>,
+    workers: Arc<Mutex<Vec<Arc<Mutex<WorkerPoolState<W>>>>>>,
 }
 
 impl<W: WebWorker> PoolExecutor<W> {
@@ -23,21 +22,22 @@ impl<W: WebWorker> PoolExecutor<W> {
     /// See [`CreateWorkerError`].
     pub fn new(initial_size: usize) -> Result<Self, CreateWorkerError> {
         Ok(Self {
-            workers: RefCell::new(
+            workers: Arc::new(Mutex::new(
                 (0..initial_size)
-                    .map(|id| Ok(Rc::new(RefCell::new(WorkerPoolState::new(id)?))))
+                    .map(|id| Ok(Arc::new(Mutex::new(WorkerPoolState::new(id)?))))
                     .collect::<Result<Vec<_>, _>>()?,
-            ),
+            )),
         })
     }
 
-    fn get_or_create_worker(&self) -> Result<Rc<RefCell<WorkerPoolState<W>>>, CreateWorkerError> {
+    fn get_or_create_worker(&self) -> Result<Arc<Mutex<WorkerPoolState<W>>>, CreateWorkerError> {
         let worker = self
             .workers
-            .borrow()
+            .lock()
+            .unwrap()
             .iter()
             .find(|w| {
-                if let Ok(mut worker) = w.try_borrow_mut() {
+                if let Ok(mut worker) = w.try_lock() {
                     if worker.available {
                         worker.available = false;
                         true
@@ -51,10 +51,11 @@ impl<W: WebWorker> PoolExecutor<W> {
             .cloned();
         let worker = worker.map_or_else(
             || {
-                let id = self.workers.borrow().len();
-                let worker: Rc<RefCell<WorkerPoolState<W>>> =
-                    Rc::new(RefCell::new(WorkerPoolState::new(id)?));
-                self.workers.borrow_mut().push(worker.clone());
+                let mut workers = self.workers.lock().unwrap();
+                let id = workers.len();
+                let worker: Arc<Mutex<WorkerPoolState<W>>> =
+                    Arc::new(Mutex::new(WorkerPoolState::new(id)?));
+                workers.push(worker.clone());
                 Ok(worker)
             },
             Ok,
@@ -74,7 +75,7 @@ impl<W: WebWorker> PoolExecutor<W> {
 /// the creation of the replaced worker before it can proceed.
 #[derive(Clone)]
 pub struct AbortHandle<W: WebWorker> {
-    worker: Weak<RefCell<WorkerPoolState<W>>>,
+    worker: Weak<Mutex<WorkerPoolState<W>>>,
     generation: usize,
 }
 
@@ -87,7 +88,7 @@ impl<W: WebWorker> AbortHandle<W> {
     /// As such, this is not expected to panic for users of the crate.
     pub fn abort(&self) {
         if let Some(ptr) = self.worker.upgrade() {
-            let mut worker = ptr.borrow_mut();
+            let mut worker = ptr.lock().unwrap();
             if !worker.available && worker.generation == self.generation {
                 worker.handle.terminate();
 
@@ -141,14 +142,14 @@ impl<W: FutureWorker> PoolExecutor<W> {
     ) -> Result<(AbortHandle<W>, impl Future<Output = W::Response>), CreateWorkerError> {
         let worker = self.get_or_create_worker()?;
         let abort_handle = {
-            let mut w = worker.borrow_mut();
+            let mut w = worker.lock().unwrap();
             w.generation += 1;
             AbortHandle {
-                worker: Rc::downgrade(&worker),
+                worker: Arc::downgrade(&worker),
                 generation: w.generation,
             }
         };
-        let mut handle = worker.borrow_mut().handle.clone();
+        let mut handle = worker.lock().unwrap().handle.clone();
         Ok((
             abort_handle,
             release_worker_after_future(worker, async move { handle.run(&request).await }),
@@ -166,10 +167,10 @@ impl<W: StreamWorker> PoolExecutor<W> {
         request: &W::Request,
     ) -> Result<(AbortHandle<W>, impl Stream<Item = W::Response>), CreateWorkerError> {
         let worker = self.get_or_create_worker()?;
-        let mut w = worker.borrow_mut();
+        let mut w = worker.lock().unwrap();
         w.generation += 1;
         let abort_handle = AbortHandle {
-            worker: Rc::downgrade(&worker),
+            worker: Arc::downgrade(&worker),
             generation: w.generation,
         };
         Ok((
@@ -191,14 +192,14 @@ impl<W: CallbackWorker> PoolExecutor<W> {
     ) -> Result<(AbortHandle<W>, impl Future<Output = ()>), CreateWorkerError> {
         let worker = self.get_or_create_worker()?;
         let abort_handle = {
-            let mut w = worker.borrow_mut();
+            let mut w = worker.lock().unwrap();
             w.generation += 1;
             AbortHandle {
-                worker: Rc::downgrade(&worker),
+                worker: Arc::downgrade(&worker),
                 generation: w.generation,
             }
         };
-        let mut handle = worker.borrow_mut().handle.clone();
+        let mut handle = worker.lock().unwrap().handle.clone();
         Ok((
             abort_handle,
             release_worker_after_future(worker, async move {
@@ -227,11 +228,10 @@ impl<W: ChannelWorker> PoolExecutor<W> {
         let worker = self.get_or_create_worker()?;
         let (proxy_tx, proxy_rx) = flume::unbounded();
         let (abort_handle, (worker_tx, worker_rx)) = {
-            let mut w = worker.borrow_mut();
-
+            let mut w = worker.lock().unwrap();
             w.generation += 1;
             let abort_handle = AbortHandle {
-                worker: Rc::downgrade(&worker),
+                worker: Arc::downgrade(&worker),
                 generation: w.generation,
             };
             (abort_handle, w.handle.channel(init))
@@ -243,30 +243,30 @@ impl<W: ChannelWorker> PoolExecutor<W> {
                 }
             }
             // TODO: is this correct? this and other sending error needs testing
-            worker.borrow_mut().available = true;
+            worker.lock().unwrap().available = true;
         });
         Ok((abort_handle, proxy_tx, worker_rx))
     }
 }
 
 fn release_worker_after_future<W: WebWorker, T>(
-    worker: Rc<RefCell<WorkerPoolState<W>>>,
+    worker: Arc<Mutex<WorkerPoolState<W>>>,
     future: impl Future<Output = T>,
 ) -> impl Future<Output = T> {
     async move {
         let result = future.await;
-        worker.borrow_mut().available = true;
+        worker.lock().unwrap().available = true;
         result
     }
 }
 
 fn release_worker_after_stream<W: WebWorker, T>(
-    worker: Rc<RefCell<WorkerPoolState<W>>>,
+    worker: Arc<Mutex<WorkerPoolState<W>>>,
     stream: impl Stream<Item = T>,
 ) -> impl Stream<Item = T> {
     // this sentinel makes sure we make the worker available only after the stream is done
     let availability_sentinel = Box::pin(futures::stream::unfold(worker, |worker| async move {
-        worker.borrow_mut().available = true;
+        worker.lock().unwrap().available = true;
         None
     }));
     stream.chain(availability_sentinel)
