ac275f6a89ccc52a30e505b1c2bab09b62723f48
Zak Stucke <zakstucke@hotmail.co.uk>
2024-07-29 11:23:52 +0100

    Localised all global store stuff into 2 fns with warnings

--- leptos_workers/src/worker_message/transferable_types.rs
+++ leptos_workers/src/worker_message/transferable_types.rs
@@ -1,23 +1,30 @@
 use wasm_bindgen::{JsCast, JsValue};
 
 /// A trait for implementing the types that can be transferred.
-/// 
-/// Some JS values can be sent to/from workers via structured cloning, others via transferables. 
-/// This trait allows configuration of those js values that shouldn't be serialized with serde, 
+///
+/// Some JS values can be sent to/from workers via structured cloning, others via transferables.
+/// This trait allows configuration of those js values that shouldn't be serialized with serde,
 /// but preserved as js.
-/// 
-/// Some [`js_sys`] types are implemented by leptos_workers internally, 
+///
+/// Some [`js_sys`] types are implemented by leptos_workers internally,
 /// other's can be PR'd or wrapped in structs downstream in user code.
-/// 
+///
 /// Structured clone spec:
 /// <https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm>
-/// 
+///
 /// Transferable spec:
 /// <https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Transferable_objects#supported_objects>
 pub trait TransferableType: std::fmt::Debug + Clone {
     #[allow(async_fn_in_trait)]
     /// Extract the underlying object that needs to be passed separately during the postMessage call.
-    /// (if any)
+    /// 
+    /// This might be None, if the object is a structured clone type rather than a transferable type. 
+    /// 
+    /// Structured clone docs: 
+    /// https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
+    /// 
+    /// Transferable docs: 
+    /// https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Transferable_objects
     async fn underlying_transfer_object(&self) -> Option<JsValue>;
 
     /// Convert a generic js value back to the original type.
