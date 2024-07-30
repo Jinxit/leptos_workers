0c957ed0e80be66805cf962c1c7aa3089aad765f
Zak Stucke <zakstucke@hotmail.co.uk>
2024-07-30 17:13:03 +0100

    base level stable types have type trait implemented

--- leptos_workers/src/messages/transferable_types/base.rs
+++ leptos_workers/src/messages/transferable_types/base.rs
@@ -4,7 +4,7 @@ use wasm_bindgen::{JsCast, JsValue};
 ///
 /// Such objects utilise the separate second argument of the postMessage webworker API dedicated to transferable objects.
 ///
-/// All basic types supporting transfers mentioned in mozilla docs are built-in,
+/// All basic types stable in js-sys/web-sys supporting transfers mentioned in mozilla docs are built-in,
 /// along with the most common derivatives like [`js_sys::Uint8Array`].
 ///
 /// [`Vec<T>`], [`std::collections::HashMap<String, T>`], [`Option<T>`] are also implemented, as long as T implements [`TransferableType`].
@@ -42,16 +42,40 @@ pub trait TransferableType: std::fmt::Debug + Clone {
     fn from_js_value(value: JsValue) -> Self;
 }
 
-impl TransferableType for js_sys::ArrayBuffer {
-    fn underlying_transfer_objects(&self) -> Vec<JsValue> {
-        vec![self.into()]
-    }
+macro_rules! impl_transferable_type {
+    ($type:ty) => {
+        impl TransferableType for $type {
+            fn underlying_transfer_objects(&self) -> Vec<JsValue> {
+                vec![self.into()]
+            }
 
-    fn from_js_value(value: JsValue) -> Self {
-        value.unchecked_into()
-    }
+            fn from_js_value(value: JsValue) -> Self {
+                value.unchecked_into()
+            }
 
-    fn to_js_value(&self) -> JsValue {
-        self.into()
-    }
+            fn to_js_value(&self) -> JsValue {
+                self.into()
+            }
+        }
+    };
 }
+
+// Basic supported types:
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Transferable_objects#supported_objects
+impl_transferable_type!(js_sys::ArrayBuffer);
+impl_transferable_type!(web_sys::MessagePort);
+impl_transferable_type!(web_sys::ReadableStream);
+impl_transferable_type!(web_sys::WritableStream);
+impl_transferable_type!(web_sys::TransformStream);
+impl_transferable_type!(web_sys::ImageBitmap);
+impl_transferable_type!(web_sys::OffscreenCanvas);
+impl_transferable_type!(web_sys::RtcDataChannel);
+
+// Still unstable: https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.AudioData.html
+// impl_transferable_type!(web_sys::AudioData);
+// Still unstable: https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.VideoFrame.html
+// impl_transferable_type!(web_sys::VideoFrame);
+// Still unstable: https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.WebTransportReceiveStream.html
+// impl_transferable_type!(web_sys::WebTransformReceiveStream);
+// Still unstable: https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.WebTransportSendStream.html
+// impl_transferable_type!(web_sys::WebTransformSendStream);
