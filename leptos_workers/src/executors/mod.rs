c16ec011d127125c5bd3f6eaecb931185ca2f422
Lucas Åström <lucas.astrom00@gmail.com>
2025-02-20 17:17:37 +0100

    Remove SingleExecutor

--- leptos_workers/src/executors/mod.rs
+++ leptos_workers/src/executors/mod.rs
@@ -2,9 +2,7 @@
 //!
 //! Executors know how to handle execution of different worker types.
 //! They do not share an explicit trait, as their implementations may differ,
-//! but the intent is that the mostly mirror the respective worker traits.
+//! but the intent is that they mostly mirror the respective worker traits.
 
 mod pool_executor;
 pub use pool_executor::*;
-mod single_executor;
-pub use single_executor::*;
